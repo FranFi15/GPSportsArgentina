30d3851632145507b89052c59240fe166bc8bf69
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2025-05-25 19:59:00 -0300

    backagregado a la ruta

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+const API_BASE_URL = import.meta.env.VITE_API_URL
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Reemplaza con la URL de tu backend
+        target: API_BASE_URL, // Reemplaza con la URL de tu backend
         changeOrigin: true,
         //rewrite: (path) => path.replace(/^\/api/, ''),
       },
