dbf7e6b82725b42c3a87edba4c921b3ff132aeb5
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2025-05-28 11:55:40 -0300

    autenticacion eliminadoa de reordenamiento

--- backend/routes/personasRoutes.js
+++ backend/routes/personasRoutes.js
@@ -2,12 +2,10 @@
 import express from 'express'; // <-- Cambio aquí
 import Jugador from '../models/Jugador.js'; // <-- ¡Importante! Usar .js si no configuras un alias
 import Entrenador from '../models/Entrenador.js'; // <-- ¡Importante! Usar .js si no configuras un alias
-import auth from '../middleware/authMiddleware.js'; // <-- Cambio aquí (asumiendo que tu middleware es auth.js)
-
 const router = express.Router();
 
 // Ruta para actualizar el orden de las personas
-router.put('/reorder', auth, async (req, res) => {
+router.put('/reorder', async (req, res) => {
   try {
     const { order } = req.body;
 
