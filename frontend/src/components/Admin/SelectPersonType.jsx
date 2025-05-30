a2c5e7759a8303072f1a26d49be1c06a9ffb374c
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2025-05-30 14:56:21 -0300

    back to rout one

--- frontend/src/components/Admin/SelectPersonType.jsx
+++ frontend/src/components/Admin/SelectPersonType.jsx
@@ -14,19 +14,13 @@ const SelectPersonType = () => {
     navigate('/admin/agregar/entrenador');
   };
 
-  // Nueva función para navegar a la gestión de redes sociales
-  const handleGoToSocialPosts = () => {
-    navigate('/admin/socialposts');
-  };
-
   return (
     <div className="select-person-type-container">
-      <h2>¿Qué deseas hacer?</h2> {/* Cambiamos el texto para que sea más general */}
+      <h2>¿Qué deseas agregar?</h2>
       <div className="buttons-container">
         <button className="add-jugador-button" onClick={handleAddJugador}>Agregar Jugador</button>
         <button className="add-entrenador-button" onClick={handleAddEntrenador}>Agregar Entrenador</button>
-        <button className="social-posts-button" onClick={handleGoToSocialPosts}>Gestionar Redes Sociales</button> {/* ¡NUEVO BOTÓN! */}
-        <button className="cancel-button" onClick={() => navigate('/admin')}>Volver al Dashboard</button> {/* Cambiamos "Cancelar" por "Volver" */}
+        <button className="cancel-button" onClick={() => navigate('/admin')}>Cancelar</button>
       </div>
     </div>
   );
