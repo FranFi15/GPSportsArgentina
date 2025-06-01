57dfd7788ffc85ea058037d649faeadcdc3dc954
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2025-06-01 18:06:47 -0300

    servida

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// =========================================================
+
 // DEPURACIÓN: AÑADIR CONSOLE.LOGS AQUÍ
 console.log('Backend iniciando...');
 console.log('NODE_ENV:', process.env.NODE_ENV);
